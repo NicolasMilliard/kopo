98654d43b6a9e3cf8689dfa1ecb8821d7472ac58
Cizeon <admin@cizeon.xyz>
2022-11-24 09:53:22 +0100

    Moved artifacts folder.

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -5,7 +5,10 @@ require('solidity-coverage');
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: '0.8.17',
-
+  paths: {
+    sources: './contracts',
+    artifacts: '../client/src/artifacts',
+  },
   defaultNetwork: 'hardhat',
   networks: {
     hardhat: { chainId: 1337 },
