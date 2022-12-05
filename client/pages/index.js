16d8c8ff99c49425c00517fbe74a06ac285d9d52
NicolasMilliard <contact@nicolasmilliard.fr>
2022-12-05 13:59:24 +0100

    Update structure

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,18 @@
-import ContractTest from '../components/ContractTest';
+import Link from 'next/link';
+import { toast } from 'react-toastify';
 
 const Index = () => {
+  const notify = () => toast('Hello Kopo!');
   return (
     <>
-      <ContractTest />
+      <h1>Landing Page</h1>
+      <Link href="/dashboard">Go to Dashboard</Link>
+      <button
+        onClick={notify}
+        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Toastify test
+      </button>
     </>
   );
 };
