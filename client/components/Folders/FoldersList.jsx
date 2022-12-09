10c75a0f07648d94538a58c602f4469a8e4a1219
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-12-10 00:08:46 +0100

    Init commit

---
+++ client/components/Folders/FoldersList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Link from 'next/link';
+
+const FoldersList = ({ folders }) => {
+  return (
+    <div className='flex flex-wrap mt-20 mb-8'>
+      {folders.map(folder => (
+        <Link key={folder.folderId} href={`/folders/${folder.newFolder}`}>
+          <div className='kopo-folder-container mr-8 mb-8 hover:drop-shadow-md'>
+            <div className='kopo-folder-wrapper'>
+              <h3 className='kopo-folder-title'>{folder.folderId.slice(0, 5)}...{folder.folderId.slice(folder.folderId.length - 4)}</h3>
+              <p className='kopo-folder-step'></p>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+export default FoldersList
