db2b842dfb00fd26af2f2d00511cd82556ba40e9
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-12-13 16:29:04 +0100

    Remove unnecessary comment

--- client/components/Buttons/ButtonLoader.jsx
+++ client/components/Buttons/ButtonLoader.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 const ButtonLoader = () => {
   return (
-    // Aspect determine if the button is full rounded or half rounded
     <button className="mt-4 mr-8 bg-gray-500 text-white font-bold py-2 px-4 rounded-xl drop-shadow-md">
       Transaction en cours...
     </button>
