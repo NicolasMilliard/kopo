4ad58c5631737f3f2c46f40a381fd20c9dfdbb29
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-12-13 20:52:13 +0100

    Accept custom text and additional classes

--- client/components/Buttons/ButtonLoader.jsx
+++ client/components/Buttons/ButtonLoader.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
-const ButtonLoader = () => {
+const ButtonLoader = ({ text, additionalClasses }) => {
+  console.log(additionalClasses);
   return (
-    <button className="mt-4 mr-8 bg-gray-500 text-white font-bold py-2 px-4 rounded-xl drop-shadow-md">
-      Transaction en cours...
-    </button>
+    <div className={`bg-green-700 text-white font-bold py-2 px-4 rounded-xl drop-shadow-md ${additionalClasses}`}>
+      <span className='flex items-center'>
+        <span className='kopo-spin-loader'></span>{text}
+      </span>
+    </div>
   )
 }
 
