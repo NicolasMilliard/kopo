e3cd5ef1e671ac10b5aa416e2904f690d0d24a94
Cizeon <admin@cizeon.xyz>
2022-12-13 11:27:13 +0100

    New Tab

--- client/components/Folders/MintFolder.jsx
+++ client/components/Folders/MintFolder.jsx
@@ -95,6 +95,7 @@ const MintFolder = ({ folderAddress, folderId, folderName }) => {
           <div className="mt-4">
             <Link
               href={openSea}
+              target="_blank"
               className="bg-green-500 text-white font-bold py-2 px-4 rounded-xl drop-shadow-md hover:bg-green-700 hover:drop-shadow-lg"
             >
               Voir le NFT sur OpenSea (testnet)
